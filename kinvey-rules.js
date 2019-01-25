167d145bcf62984e8789fcfde78b6ee666c170d2
Dimo Mitev <dimo.mitev@telerik.com>
2019-01-25 08:14:54 +0200

    Update kinvey-rules.js

--- kinvey-rules.js
+++ kinvey-rules.js
@@ -493,7 +493,7 @@ module.exports = {
     "no-negated-condition": "off",
     "no-nested-ternary": "error",
     "no-new-object": "error",
-    "no-plusplus": "error",
+    "no-plusplus": "off",
     "no-restricted-syntax": [
       "warn",
       "ForInStatement",
